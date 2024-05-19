a08164408b2a45d8f3458d5b7c402fab3a826596
KurryW <kwesterhuis@hotmail.com>
2024-05-19 14:51:34 +0200

    Waypoint marker

--- LevelDesign/Assets/Scripts/AcceptMission.cs
+++ LevelDesign/Assets/Scripts/AcceptMission.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class AcceptMission : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Marker;
+    public GameObject MissionUI;
 
-    // Update is called once per frame
-    void Update()
+    public void MissionAccepted()
     {
-        
+        MissionUI.SetActive(false);
+        Marker.SetActive(true);
     }
 }
